3214b61b19a73e3a5d5834ef4723df439b705da3
KokilaGit <kokila_thirugnanam@yahoo.com>
2023-07-11 14:08:45 -0600

    Add files via upload

---
+++ src/main/java/com/vasc/vasc_api/VascApiApplication.java
@@ -0,0 +1,13 @@
+package com.vasc.vasc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VascApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VascApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasc/vasc_api/VascApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasc.vasc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VascApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
