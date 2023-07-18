a80f704e1e8e8296fee3b5f448504e90b59e6692
“KokilaGit” <kokila_thirugnanam@yahoo.com>
2023-07-18 13:50:55 -0500

    added checkout button in the cart table

--- target/classes/static/Services/service.js
+++ target/classes/static/Services/service.js
@@ -12,6 +12,52 @@ async function getProducts() {
   }
 }
 
+//cartFunction
+async function cartList() {
+  let cartListResponse = await fetch("/api/cart/user/1", {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+});
+
+let cartListStatus = cartListResponse.ok;
+if (cartListStatus) {
+  let productLists = await cartListResponse.json();
+  console.log("hello");
+  console.log(productLists);
+  console.log("Product:" + productLists[0].product.title);
+  let cartDisplay = document.getElementById("product-table");
+  let deleteBody = document.getElementsByClassName("demoClass")[0];
+  if (deleteBody != null) {
+    deleteBody.remove();
+  }
+  let tableBody = document.createElement("tbody");
+  tableBody.setAttribute("class", "demoClass");
+  cartDisplay.append(tableBody);
+  let total = 0;
+  productLists.forEach((productList) => {
+    let newRow = document.createElement("tr");
+
+    newRow.innerHTML = `
+                              <td>${productList.product.title}</td><td></td>
+                              <td>$${productList.product.price}</td>`;
+    total += Number(productList.product.price);
+    tableBody.append(newRow);
+  });
+  let totalRow = document.createElement("tr");
+  totalRow.innerHTML = ` <td><b>Total</b></td>
+                        <td></td>
+                         <td>$${total}</td>
+                         `;
+  tableBody.append(totalRow);
+  let checkoutRow = document.createElement("tr");
+checkoutRow.innerHTML = '<td class="chechkout-btn"><button onclick="myFunc()">Checkout</button></td>';
+tableBody.appendChild(checkoutRow);
+} else {
+  alert("Error: Could not add product");
+}
+}
+
+
 (async () => {
   let serviceArray = await getProducts();
   console.log(serviceArray);
@@ -34,59 +80,49 @@ async function getProducts() {
   console.log(selectServiceBtn);
   selectServiceBtn.forEach((btn) => {
     btn.addEventListener("click", async function (event) {
-      let cart = {
-        product_id: Number(event.target.getAttribute("data-pid")),
-        user_id: 1,
-      };
-
-      let response = await fetch("/api/cart/add", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(cart),
-      });
-      const responseStatus = response.ok;
-      if (responseStatus) {
-        let addedProduct = await response.json();
-      } else {
-        alert("Error: Could not add player");
-      }
-
-      let cartListResponse = await fetch("/api/cart/user/1", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
-
-      let cartListStatus = cartListResponse.ok;
-      if (cartListStatus) {
-        let productLists = await cartListResponse.json();
-        console.log("hello");
-        console.log(productLists);
-        console.log("Product:" + productLists[0].product.title);
-        let cartDisplay = document.getElementById("product-table");
-        let deleteBody = document.getElementsByClassName("demoClass")[0];
-        if (deleteBody != null) {
-          deleteBody.remove();
-        }
-        let tableBody = document.createElement("tbody");
-        tableBody.setAttribute("class", "demoClass");
-        cartDisplay.append(tableBody);
-        let total = 0;
-        productLists.forEach((productList) => {
-          let newRow = document.createElement("tr");
-          newRow.innerHTML = `
-                                      <td>${productList.product.title}</td><td></td>
-                                      <td>$${productList.product.price}</td>`;
-          total += Number(productList.product.price);
-          tableBody.append(newRow);
+      try {
+        //duplicate checking
+        let duplicateResponse = await fetch("/api/cart/user/1", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
         });
-        let totalRow = document.createElement("tr");
-        totalRow.innerHTML = ` <td><b>Total</b></td>
-                                <td></td>
-                                 <td>$${total}</td>`;
-        tableBody.append(totalRow);
-      } else {
-        alert("Error: Could not add player");
+        let duplicateCartList = [];
+        let duplicateStatus = duplicateResponse.ok;
+        if (duplicateStatus) {
+          duplicateCartList = await duplicateResponse.json();
+        } else {
+          alert("Error: Couldn't call cart api");
+        }
+        duplicateCartList.forEach((duplicateList) => {
+          if (duplicateList.product.id === Number(event.target.getAttribute("data-pid"))) {
+            throw "Item already added!";
+          }
+        });
+
+        let cart = {
+          product_id: Number(event.target.getAttribute("data-pid")),
+          user_id: 1,
+        };
+
+        let response = await fetch("/api/cart/add", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(cart),
+        });
+        const responseStatus = response.ok;
+        if (responseStatus) {
+          let addedProduct = await response.json();
+        } else {
+          alert("Error: Could not add player");
+        }
+        cartList();
+      } catch (err) {
+        alert(err);
       }
     });
   });
 })();
+function myFunc() {
+  alert("Your order has been placed")
+}
+window.onload(cartList());
