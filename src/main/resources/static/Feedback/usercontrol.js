ff7e512e553aee650a794730f7a6749e2cc4271f
Dharinya Ayyappan <kalai.ayya28@gmail.com>
2023-07-18 09:16:42 -0500

    finalAdding

--- src/main/resources/static/Feedback/usercontrol.js
+++ src/main/resources/static/Feedback/usercontrol.js
@@ -2,13 +2,27 @@ const userController = new UserController;
 const userFeed = document.getElementById("form");
  userFeed.addEventListener("submit" ,function(event)
  {
+ console.log('selvi');
     event.preventDefault()
     const fName = document.getElementById("fName").value;
-    const lName = document.getElementById("fName").value;
+    const lName = document.getElementById("lName").value;
     const email = document.getElementById("email").value;
     const feedback =  document.getElementById("feedback").value;
     userController.addUser(fName,lName,email,feedback)
     userController.setLocalStorage()
-    userFeed.reset()
+
+    let fbObject = {} ;
+
+    fbObject.firstName = fName;
+    fbObject.lastName = lName;
+    fbObject.emailId = email;
+    fbObject.feedBack = feedback;
+
+
+    console.log('fbObject ' + JSON.stringify(fbObject));
+
+    userController.postFb(fbObject);
+    userFeed.reset();
+
  })
-       
+
